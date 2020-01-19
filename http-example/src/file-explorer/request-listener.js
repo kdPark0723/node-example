a04bb3aeb32c383e666b5c07b405b990377a57c3
박시율 <kdpark0723@gmail.com>
2020-01-19 18:55:48 +0900

    리팩토링

--- http-example/src/file-explorer/request-listener.js
+++ http-example/src/file-explorer/request-listener.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const url = require('url');
 
 function requestListener(request, response) {
+  if (request.method !== 'GET') {
+    response.writeHead(405, { 'Content-Type': 'text/html' });
+    response.end('<h1>Method Not Allowed</h1>');
+
+    return;
+  }
+
   let pathname = url.parse(request.url).pathname;
   if (pathname ==="/") pathname = "/index.html";
 
@@ -16,8 +23,11 @@ function requestListener(request, response) {
     response.end('<h1>Not Found</h1>');
   });
 
+  fileSteam.on('open', () => {
+    console.log(`[${client.address}:${client.port}]: Send ${pathname} Start.`);
+  });
   fileSteam.on('close', () => {
-    console.log(`[${client.address}:${client.port}]: Request Get ${pathname} Finish.`);
+    console.log(`[${client.address}:${client.port}]: Send ${pathname} Finish.`);
   });
 
   fileSteam.pipe(response);
